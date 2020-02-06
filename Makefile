b2a0d85724347fc9818c980457f8cd2e7c96cd87
Laura Fitzgerald <lfitzger@redhat.com>
2020-02-06 10:46:01 +0000

    add dependencies/permissions to dockerfile

--- Makefile
+++ Makefile
@@ -1,9 +1,17 @@
 SHELL=/bin/bash
+GIT_REF=$(shell sh -c "git rev-parse --short HEAD")
+export TOWER_OPENSHIFT_PROJECT=tower-e2e-${GIT_REF}
+
+.PHONY test/e2e/prow:
+test/e2e/prow: export TOWER_OPENSHIFT_PROJECT=tower-e2e-${OPENSHIFT_BUILD_NAMESPACE}
+test/e2e/prow: export OPENSHIFT_MASTER=$(shell cat /usr/local/integr8ly-tower-secrets/OPENSHIFT_MASTER)
+test/e2e/prow: export TOWER_LICENSE=$(shell cat /usr/local/integr8ly-tower-secrets/TOWER_LICENSE)
+test/e2e/prow: export TOWER_OPENSHIFT_PASSWORD=$(shell cat /usr/local/integr8ly-tower-secrets/TOWER_OPENSHIFT_PASSWORD)
+test/e2e/prow: export TOWER_OPENSHIFT_USERNAME=$(shell cat /usr/local/integr8ly-tower-secrets/TOWER_OPENSHIFT_USERNAME)
+test/e2e/prow: export TOWER_PASSWORD=$(shell cat /usr/local/integr8ly-tower-secrets/TOWER_PASSWORD)
+test/e2e/prow: export TOWER_USERNAME=$(shell cat /usr/local/integr8ly-tower-secrets/TOWER_USERNAME)
+test/e2e/prow: test/e2e
 
 .PHONY test/e2e:
 test/e2e:
-	echo "e2e test will go here"
-
-.PHONY test/e2e/local:
-test/e2e/local:
 	scripts/e2e.sh
--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-git_ref=$(git rev-parse --short HEAD)
-tower_openshift_project=tower-e2e-"$git_ref"
-
-echo "tower_openshift_project: '"$tower_openshift_project"'" >> e2e_test_config.yml
+echo "tower_openshift_project: '"$TOWER_OPENSHIFT_PROJECT"'" >> e2e_test_config.yml
 echo "tower_openshift_password: '"$TOWER_OPENSHIFT_PASSWORD"'" >> e2e_test_config.yml
 echo "tower_openshift_username: '"$TOWER_OPENSHIFT_USERNAME"'" >> e2e_test_config.yml
 echo "tower_openshift_master_url: '"$OPENSHIFT_MASTER"'" >> e2e_test_config.yml
 
-echo "Installing tower on openshift: ${OPENSHIFT_MASTER}"
+if [ -f /.dockerenv ]; then
+    echo "tempuser:x:$(id -u):$(id -g):,,,:${HOME}:/bin/bash" >> /etc/passwd
+    echo "tempuser:x:$(id -G | cut -d' ' -f 2)" >> /etc/group
+fi
+
+echo "Installing tower on openshift: ${TOWER_OPENSHIFT_PROJECT}"
 ansible-playbook -i ./inventories/hosts \
 playbooks/install_tower.yml \
 --extra-vars "@e2e_test_config.yml"
@@ -20,7 +22,7 @@ echo "e2e_tower_license: '"$TOWER_LICENSE"'" >> e2e_test_config.yml
 echo "e2e_tower_password: '"$TOWER_PASSWORD"'" >> e2e_test_config.yml
 echo "e2e_tower_username: '"$TOWER_USERNAME"'" >> e2e_test_config.yml
 
-echo "Bootstrapping tower in openshift project: $tower_openshift_project"
+echo "Bootstrapping tower in openshift project: ${TOWER_OPENSHIFT_PROJECT}"
 ansible-playbook -i ./inventories/hosts \
 playbooks/bootstrap.yml \
 --extra-vars "@e2e_test_config.yml"
@@ -28,6 +30,6 @@ bootstrap_exit_code=$?
 echo "Bootstrap playbook ansible exit code: $bootstrap_exit_code"
 
 
-oc delete project $tower_openshift_project
+oc delete project ${TOWER_OPENSHIFT_PROJECT}
 exit_code=`expr $install_exit_code + $bootstrap_exit_code`
 exit $exit_code
