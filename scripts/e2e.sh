2fcbbd1e02d841bbaf979bc6e03cf465846b8378
Laura Fitzgerald <lfitzger@redhat.com>
2020-02-07 14:27:35 +0000

    cleanup properly

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
+cleanup (){
+    oc login $OPENSHIFT_MASTER -u $TOWER_OPENSHIFT_USERNAME -p $TOWER_OPENSHIFT_PASSWORD
+    oc delete project ${TOWER_OPENSHIFT_PROJECT}
+}
+
+trap 'cleanup' EXIT
+
 echo "tower_openshift_project: '"$TOWER_OPENSHIFT_PROJECT"'" >> e2e_test_config.yml
 echo "tower_openshift_password: '"$TOWER_OPENSHIFT_PASSWORD"'" >> e2e_test_config.yml
 echo "tower_openshift_username: '"$TOWER_OPENSHIFT_USERNAME"'" >> e2e_test_config.yml
@@ -14,8 +23,6 @@ echo "Installing tower on openshift: ${TOWER_OPENSHIFT_PROJECT}"
 ansible-playbook -i ./inventories/hosts \
 playbooks/install_tower.yml \
 --extra-vars "@e2e_test_config.yml"
-install_exit_code=$?
-echo "Install playbook ansible exit code: $install_exit_code"
 
 #Pass environment variables to config file
 echo "e2e_tower_license: '"$TOWER_LICENSE"'" >> e2e_test_config.yml
@@ -26,10 +33,4 @@ echo "Bootstrapping tower in openshift project: ${TOWER_OPENSHIFT_PROJECT}"
 ansible-playbook -i ./inventories/hosts \
 playbooks/bootstrap.yml \
 --extra-vars "@e2e_test_config.yml"
-bootstrap_exit_code=$?
-echo "Bootstrap playbook ansible exit code: $bootstrap_exit_code"
 
-
-oc delete project ${TOWER_OPENSHIFT_PROJECT}
-exit_code=`expr $install_exit_code + $bootstrap_exit_code`
-exit $exit_code
